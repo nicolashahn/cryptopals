a544c7fab3a73c76f0d8598bd6720a00cbd61bc8
Nicolas Hahn <nicolas@stonespring.org>
2018-07-08 14:54:13 -0400

    fix var name in repeating_key_xor

--- set1.py
+++ set1.py
@@ -134,12 +134,12 @@ res = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a262263242727652
       "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
 def repeating_key_xor(string, key="ICE"):
-    ice_cycle = cycle([ord(c) for c in key])
+    key_cycle = cycle([ord(c) for c in key])
     ords = [ord(x) for x in string]
     res = []
     for o in ords:
-        ice_char = ice_cycle.next()
-        xored = o ^ ice_char
+        key_char = key_cycle.next()
+        xored = o ^ key_char
         res.append(xored)
     res_str = ''.join([chr(o).encode("hex") for o in res])
     return res_str
