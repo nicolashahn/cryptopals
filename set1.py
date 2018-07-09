e0c0b25e40c7fd0151cd783c5777b959e6844347
Nicolas Hahn <nicolas@stonespring.org>
2018-07-08 20:15:05 -0400

    challenge 6

--- set1.py
+++ set1.py
@@ -37,6 +37,7 @@ def xor_bufs(buf1, buf2):
 from collections import Counter
 
 message = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+decrypted = "Cooking MC's like a pound of bacon"
 
 letterFrequency = {'E' : 12.0,
 'T' : 9.10,
@@ -87,23 +88,21 @@ def score_str(string):
         score += c_score
     return score
 
-def xor_buf_with_char(buf, i):
-    ords = [ord(x) for x in binascii.unhexlify(buf)]
-    xors = [o^i for o in ords]
-    chrs = [chr(c) for c in xors]
-    return ''.join(chrs)
-
-
-def best_guess_decryption(buf):
+def best_guess_decryption_ords(ords):
     triples = []
     for i in range(256):
-        string = xor_buf_with_char(buf, i)
+        xors = [o^i for o in ords]
+        string = ''.join([chr(i) for i in xors])
         score = score_str(string)
         triples.append((score, string, i))
     s_triples = reversed(sorted(triples, key=lambda k: k[0]))
     return s_triples.next()
     
-# print best_guess_decryption(message)
+def best_guess_decryption_hex_str(buf):
+    ords = [ord(i) for i in binascii.unhexlify(buf)]
+    return best_guess_decryption_ords(ords)
+
+assert best_guess_decryption_hex_str(message)[1] == decrypted
 
 # Challenge 4
 
@@ -114,13 +113,13 @@ def challenge4():
         lines = f.readlines()
         decrypts = []
         for line in tqdm(lines):
-            decrypts.append(best_guess_decryption(line.strip()))
+            decrypts.append(best_guess_decryption_hex_str(line.strip()))
 
         s_decrypts = reversed(sorted(decrypts, key=lambda k: k[0]))
 
-        print s_decrypts.next()
+        return s_decrypts.next()[1]
 
-# challenge4()
+# assert challenge4() == 'Now that the party is jumping\n'
 
 
 # Challenge 5
@@ -131,7 +130,7 @@ string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a c
 res = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"+\
       "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-def repeating_key_xor(string, key="ICE"):
+def repeating_key_xor(string, key="ICE", hex_encode=False):
     key_cycle = cycle([ord(c) for c in key])
     ords = [ord(x) for x in string]
     res = []
@@ -139,10 +138,13 @@ def repeating_key_xor(string, key="ICE"):
         key_char = key_cycle.next()
         xored = o ^ key_char
         res.append(xored)
-    res_str = ''.join([chr(o).encode("hex") for o in res])
-    return res_str
+    if hex_encode:
+        chars = [chr(o).encode("hex") for o in res]
+    else:
+        chars = [chr(o) for o in res]
+    return ''.join(chars)
 
-assert repeating_key_xor(string) == res
+assert repeating_key_xor(string, hex_encode=True) == res
 
 
 # Challenge 6
@@ -182,7 +184,25 @@ def challenge6():
         ords = [ord(x) for x in string.decode("base64")]
         keysize = get_keysize(ords)
         chunks = [ords[i:i + keysize] for i in xrange(0, len(ords), keysize)]
-        return chunks
-
+        blocks = {i: [] for i in range(keysize)}
+        for chunk in chunks:
+            for i in range(len(chunk)):
+                blocks[i].append(chunk[i])
+        key_ords = []
+        d_blocks = {i: [] for i in range(keysize)}
+        for i in sorted(blocks.keys()):
+            s_triple = best_guess_decryption_ords(blocks[i])
+            d_blocks[i] = list(s_triple[1])
+            key_ords.append(s_triple[2])
+        res = ''
+        while d_blocks:
+            for i in sorted(d_blocks.keys()):
+                res += d_blocks[i].pop(0)
+            for i in d_blocks.keys():
+                if not d_blocks[i]:
+                    del d_blocks[i]
+        # key = ''.join([chr(c) for c in key_ords])
+        # return repeating_key_xor(string, key=key)
+        return res
 
 print challenge6()
